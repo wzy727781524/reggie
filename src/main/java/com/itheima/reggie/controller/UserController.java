b822c3bf3eff142ba446537fc6aa9aaa7a88e17a
wzy <727781524@qq.com>
2022-05-25 19:00:53 +0800

    Redis的环境与部署

--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -45,7 +45,7 @@ public class UserController {
             //随机生成四位的验证码
             Integer code = ValidateCodeUtils.generateValidateCode(4);
             //调用阿里云提供的短信服务API完成发生短信
-            SMSUtils.sendMessage("阿里云短信测试", "SMS_154950909", phone, code.toString());
+            //SMSUtils.sendMessage("阿里云短信测试", "SMS_154950909", phone, code.toString());
             log.info(String.valueOf(code));
 
             //需要将生成的验证码保存到Session
@@ -53,6 +53,38 @@ public class UserController {
             return R.success("手机验证码发送成功");
         }
         return R.error("短信发送失败");
+    }
+    @PostMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession session) {
+        //获取传过来的手机号
+        String phone = map.get("phone").toString();
+        //获取传过来的验证码
+        String code =  map.get("code").toString();
+
+        //获取Session里面的验证码
+        String codeInSession = session.getAttribute("code").toString();
+
+        //进行验证码对比
+        if (codeInSession != null && codeInSession.equals(code)) {
+            //验证码正确,查询用户
+            LambdaQueryWrapper<User> lw = new LambdaQueryWrapper<>();
+            lw.eq(User::getPhone, phone);
+
+            User user = userService.getOne(lw);
+            //并判断用户是否为新用户,是用户就帮他注册,否则就登录成功
+            if (user == null) {
+                //为新用户
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+
+            session.setAttribute("user",user);
+
+            return R.success(user);
+        }
+        return R.error("登录失败！");
     }
 
 //    @PostMapping("/login")
@@ -87,32 +119,34 @@ public class UserController {
 //        }
 //       return R.error("登录失败！");
 //    }
-    @PostMapping("/login")
-    public R<User> login(@RequestBody Map map, HttpSession session) {
-        //获取传过来的手机号
-        String phone = map.get("phone").toString();
-        //获取传过来的验证码
-        String code =  map.get("code").toString();
 
-        //获取Session里面的验证码
-        //String codeInSession = session.getAttribute("code").toString();
+//    @PostMapping("/login")
+//    public R<User> login(@RequestBody Map map, HttpSession session) {
+//        //获取传过来的手机号
+//        String phone = map.get("phone").toString();
+//        //获取传过来的验证码
+//        String code =  map.get("code").toString();
+//
+//        //获取Session里面的验证码
+//        //String codeInSession = session.getAttribute("code").toString();
+//
+//        //进行验证码对比
+//            //验证码正确,查询用户
+//            LambdaQueryWrapper<User> lw = new LambdaQueryWrapper<>();
+//            lw.eq(User::getPhone, phone);
+//
+//            User user = userService.getOne(lw);
+//            //并判断用户是否为新用户,是用户就帮他注册,否则就登录成功
+//            if (user == null) {
+//                //为新用户
+//                user = new User();
+//                user.setPhone(phone);
+//                user.setStatus(1);
+//                userService.save(user);
+//            }
+//            session.setAttribute("user",user);
+//            return R.success(user);
+//    }
 
-        //进行验证码对比
-            //验证码正确,查询用户
-            LambdaQueryWrapper<User> lw = new LambdaQueryWrapper<>();
-            lw.eq(User::getPhone, phone);
-
-            User user = userService.getOne(lw);
-            //并判断用户是否为新用户,是用户就帮他注册,否则就登录成功
-            if (user == null) {
-                //为新用户
-                user = new User();
-                user.setPhone(phone);
-                user.setStatus(1);
-                userService.save(user);
-            }
-            session.setAttribute("user",user);
-            return R.success(user);
-    }
 }
 
